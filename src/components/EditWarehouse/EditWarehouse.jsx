d7bc940bc0e361c71c921349b0c74b4041789176
isabella <isa99fv@gmail.com>
2024-06-25 14:43:21 -0400

    style for mobile implemented

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -16,30 +16,30 @@ const EditWarehouse = () => {
         <div className="form__section">
             <h2 className="form__subtitle">Warehouse Details</h2>
           <h3 className="form__label">Warehouse Name</h3>
-          <input className="form__input" type="text" name="warehouseName" id="warehouseName" />
+          <input className="form__input" type="text" name="warehouseName" id="warehouseName" placeholder="Warehouse Name" />
           <h3 className="form__label">Street Address</h3>
-          <input className="form__input" type="text" name="streetAddress" id="streetAddress" />
+          <input className="form__input" type="text" name="streetAddress" id="streetAddress" placeholder="Street Address" />
           <h3 className="form__label">City</h3>
-          <input className="form__input" type="text" name="city" id="city" />
+          <input className="form__input" type="text" name="city" id="city" placeholder="City" />
           <h3 className="form__label">Country</h3>
-          <input className="form__input" type="text" name="country" id="country" />
+          <input className="form__input" type="text" name="country" id="country" placeholder="Country" />
         </div>
         <hr className="edit-wh__divider edit-wh__divider--vertical" />
         <div className="form__section">
             <h2 className="form__subtitle">Contact Details</h2>
           <h3 className="form__label">Contact Name</h3>
-          <input className="form__input" type="text" name="contactName" id="contactName" />
+          <input className="form__input" type="text" name="contactName" id="contactName" placeholder="Contact Name" />
           <h3 className="form__label">Position</h3>
-          <input className="form__input" type="text" name="position" id="position" />
+          <input className="form__input" type="text" name="position" id="position" placeholder="Position" />
           <h3 className="form__label">Phone Number</h3>
-          <input className="form__input" type="tel" name="phoneNumber" id="phoneNumber" />
-          <h3 className="form__label">email</h3>
-          <input className="form__input" type="email" name="email" id="email" />
+          <input className="form__input" type="tel" name="phoneNumber" id="phoneNumber" placeholder="Phone Number" />
+          <h3 className="form__label">Email</h3>
+          <input className="form__input" type="email" name="email" id="email" placeholder="Email" />
         </div>
         </div>
         <div className="edit-wh__buttons">
-            <button className="edit-wh__submit" type="submit">Save</button>
             <Link className="edit-wh__cancel" to={"/warehouses"}>Cancel</Link>
+            <button className="edit-wh__save" type="submit">Save</button>
         </div>
       </form>
     </main>
