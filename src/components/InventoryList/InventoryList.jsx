4d0f757fa4146cc35ffaf15843777c9765b96105
Nilufar Shaikh <nilufarrehanshaikh@gmail.com>
2024-06-25 14:27:25 -0400

    Added search component in the inventory list page

---
+++ src/components/InventoryList/InventoryList.jsx
@@ -0,0 +1,9 @@
+import { Link } from "react-router-dom";
+import chevron from "../../assets/icons/chevron_right-24px.svg";
+import "./InventoryList.scss";
+
+const InventoryList = () => {
+  return <section className="inventory-list"></section>;
+};
+
+export default InventoryList;
--- src/pages/Inventory/Inventory.jsx
+++ src/pages/Inventory/Inventory.jsx
@@ -1,7 +1,18 @@
+import InventoryList from "../../components/InventoryList/InventoryList";
+import Search from "../../components/Search/Search";
+import "./Inventory.scss";
+
 const Inventory = () => {
   return (
-    <div>Inventory</div>
-  )
-}
+    <main className="main">
+      <section className="inventory">
+        <h1 className="inventory__heading">Inventory</h1>
+        <Search />
+        <hr className="divider" />
+        <InventoryList />
+      </section>
+    </main>
+  );
+};
 
-export default Inventory
+export default Inventory;
