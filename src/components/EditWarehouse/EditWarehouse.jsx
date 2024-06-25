dd04a34694aa13f5fcb3a4f2567afae47d755fd9
isabella <isa99fv@gmail.com>
2024-06-25 17:44:29 -0400

    front end form validation

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -7,22 +7,33 @@ const EditWarehouse = () => {
   const { warehouseId } = useParams();
   const [warehouse, setWarehouse] = useState(null);
   const [formData, setFormData] = useState(null);
-
+  const [errors, setErrors] = useState({
+    warehouse_name: "",
+    address: "",
+    city: "",
+    country: "",
+    contact_name: "",
+    contact_position: "",
+    contact_phone: "",
+    contact_email: "",
+  });
 
   useEffect(() => {
     const fetchWarehouse = async () => {
       try {
-        const response = await axios.get(`http://localhost:8080/api/warehouses/${warehouseId}`);
+        const response = await axios.get(
+          `http://localhost:8080/api/warehouses/${warehouseId}`
+        );
         setWarehouse(response.data);
         setFormData({
-          warehouseName: response.data.warehouse_name,
-          streetAddress: response.data.address,
+          warehouse_name: response.data.warehouse_name,
+          address: response.data.address,
           city: response.data.city,
           country: response.data.country,
-          contactName: response.data.contact_name,
-          position: response.data.contact_position,
-          phoneNumber: response.data.contact_phone,
-          email: response.data.contact_email,
+          contact_name: response.data.contact_name,
+          contact_position: response.data.contact_position,
+          contact_phone: response.data.contact_phone,
+          contact_email: response.data.contact_email,
         });
       } catch (error) {
         console.error("Error fetching warehouse data:", error);
@@ -39,6 +50,50 @@ const EditWarehouse = () => {
   const handleInputChange = (e) => {
     const { name, value } = e.target;
     setFormData({ ...formData, [name]: value });
+    setErrors({ ...errors, [name]: "" });
+  };
+
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const validateContactPhone = (contactPhone) => {
+    const re = /^(\+?\d{1,2}\s?)?(\(?\d{3}\)?[-.\s]?)?\d{3}[-.\s]?\d{4}$/;
+    ;
+    return re.test(String(contactPhone));
+  };
+
+  const validateForm = () => {
+    const newErrors = {};
+    Object.keys(formData).forEach((key) => {
+      if (!formData[key]) {
+        newErrors[key] = "This field is required";
+      }
+    });
+
+    if (
+      formData.contact_phone &&
+      !validateContactPhone(formData.contact_phone)
+    ) {
+      newErrors.contact_phone = "Invalid phone number format";
+    }
+
+    if (formData.contact_email && !validateEmail(formData.contact_email)) {
+      newErrors.contact_email = "Invalid email format";
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validateForm()) {
+      console.log("Form submitted successfully:", formData);
+      // send data to the backend server
+    }
   };
 
   return (
@@ -50,95 +105,124 @@ const EditWarehouse = () => {
         <h1 className="edit-wh__title">Edit Warehouse</h1>
       </div>
       <hr className="edit-wh__divider" />
-      <form className="form" action="">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form__sections">
           <div className="form__section">
             <h2 className="form__subtitle">Warehouse Details</h2>
             <h3 className="form__label">Warehouse Name</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.warehouse_name ? "form__input--error" : ""
+              }`}
               type="text"
-              name="warehouseName"
-              id="warehouseName"
+              name="warehouse_name"
+              id="warehouse_name"
               placeholder="Warehouse Name"
-              value={warehouse.warehouse_name}
+              value={formData.warehouse_name}
               onChange={handleInputChange}
-            
             />
+                        {errors.warehouse_name && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.warehouse_name}</p>}
+
             <h3 className="form__label">Street Address</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.address ? "form__input--error" : ""
+              }`}
               type="text"
-              name="streetAddress"
-              id="streetAddress"
+              name="address"
+              id="address"
               placeholder="Street Address"
-              value={warehouse.address}
+              value={formData.address}
               onChange={handleInputChange}
             />
+                        {errors.address && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.address}</p>}
+
             <h3 className="form__label">City</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.city ? "form__input--error" : ""
+              }`}
               type="text"
               name="city"
               id="city"
               placeholder="City"
-              value={warehouse.city}
+              value={formData.city}
               onChange={handleInputChange}
             />
+                        {errors.city && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.city}</p>}
+
             <h3 className="form__label">Country</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.country ? "form__input--error" : ""
+              }`}
               type="text"
               name="country"
               id="country"
               placeholder="Country"
-              value={warehouse.country}
+              value={formData.country}
               onChange={handleInputChange}
             />
+                        {errors.country && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.country}</p>}
+
           </div>
           <hr className="edit-wh__divider edit-wh__divider--vertical" />
           <div className="form__section">
             <h2 className="form__subtitle">Contact Details</h2>
             <h3 className="form__label">Contact Name</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.contact_name ? "form__input--error" : ""
+              }`}
               type="text"
-              name="contactName"
-              id="contactName"
+              name="contact_name"
+              id="contact_name"
               placeholder="Contact Name"
-              value={warehouse.contact_name}
+              value={formData.contact_name}
               onChange={handleInputChange}
             />
+            {errors.contact_name && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.contact_name}</p>}
             <h3 className="form__label">Position</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.contact_position ? "form__input--error" : ""
+              }`}
               type="text"
-              name="position"
-              id="position"
+              name="contact_position"
+              id="contact_position"
               placeholder="Position"
-              value={warehouse.contact_position}
+              value={formData.contact_position}
               onChange={handleInputChange}
             />
+                {errors.contact_position && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.contact_position}</p>}
+
             <h3 className="form__label">Phone Number</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.contact_phone ? "form__input--error" : ""
+              }`}
               type="tel"
-              name="phoneNumber"
-              id="phoneNumber"
+              name="contact_phone"
+              id="contact_phone"
               placeholder="Phone Number"
-              value={warehouse.contact_phone}
+              value={formData.contact_phone}
               onChange={handleInputChange}
             />
+                {errors.contact_phone && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.contact_phone}</p>}
+
             <h3 className="form__label">Email</h3>
             <input
-              className="form__input"
+              className={`form__input ${
+                errors.contact_email ? "form__input--error" : ""
+              }`}
               type="email"
-              name="email"
-              id="email"
+              name="contact_email"
+              id="contact_email"
               placeholder="Email"
-              value={warehouse.contact_email}
+              value={formData.contact_email}
               onChange={handleInputChange}
             />
+                {errors.contact_email && <p className="form__error"><img className="form__error--icon" src="" alt="" /> {errors.contact_email}</p>}
           </div>
         </div>
         <div className="edit-wh__buttons">
