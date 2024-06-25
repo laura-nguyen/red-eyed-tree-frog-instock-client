dc47ebe04a5784aa2953699b929103873fb66bfd
isabella <isa99fv@gmail.com>
2024-06-25 00:17:22 -0400

    add header component contents in jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,18 @@
+import "./Header.scss"
+import logo from "../../assets/logo/InStock-Logo_1x.png"
+import { Link, NavLink } from "react-router-dom";
+
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header">
+      <Link to={"/warehouses"} className="header__logo">
+        <img src={logo} alt="instock logo" />
+      </Link>
+      <nav className="nav">
+        <NavLink className="nav__item" to={"/warehouses"}>Warehouses</NavLink>
+        <NavLink className="nav__item" to={"/inventories"}>Inventory</NavLink>
+      </nav>
+    </header>
   )
 }
 
