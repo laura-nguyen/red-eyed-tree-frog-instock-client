54e7bb3f14e26392c306c29d60d38c0c70d78fe3
isabella <isa99fv@gmail.com>
2024-06-25 00:20:58 -0400

    add logo folder to assets

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,12 @@
 import "./Header.scss"
-// import logo from "../../assets/logo/InStock-Logo_1x.png"
+import logo from "../../assets/logo/InStock-Logo_1x.png"
 import { Link, NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
     <header className="header">
       <Link to={"/warehouses"} className="header__logo">
-        <img src="" alt="instock logo" />
+        <img src={logo} alt="instock logo" />
       </Link>
       <nav className="nav">
         <NavLink className="nav__item" to={"/warehouses"}>Warehouses</NavLink>
