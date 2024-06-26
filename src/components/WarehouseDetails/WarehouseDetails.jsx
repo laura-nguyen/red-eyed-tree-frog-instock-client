20befcf1a7490a266a155d284efcb2110115fc3b
Laura Nguyen <hello@lauranguyen.me>
2024-06-26 14:03:02 -0400

    changed svg sizing, added mobile and tablet view

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,10 +1,11 @@
 import "./WarehouseDetails.scss";
 import { Link } from "react-router-dom";
 import backArrow from "../../assets/icons/arrow_back-24px.svg";
+import editIcon from "../../assets/icons/edit-24px.svg";
 
 const WarehouseDetails = () => {
   return (
-    <main className="wrapper">
+    <main className="wh-details__wrapper">
       <div className="wh-details__header">
         <Link className="wh-details__header-btn--back" to={"/warehouses"}>
           <img
@@ -14,25 +15,32 @@ const WarehouseDetails = () => {
           />
         </Link>
         <h1 className="edit-wh__title">Washington</h1>
-        <button className="edit-wh__header-btn--edit"></button>
+        <button className="edit-wh__header-btn--edit-mobile">
+          <img src={editIcon} alt="Edit icon" />
+        </button>
+        <button className="edit-wh__header-btn--edit">
+          <img src={editIcon} alt="Edit icon" />
+          Edit
+        </button>
       </div>
-      <hr className="edit-wh__divider" />
 
-      <div className="wh-details__wrapper">
+      <div className="wh-details__info">
         <div className="wh-details__address">
-          <h4>Warehouse Address:</h4>
-          <p>Washington, USA</p>
+          <h4 className="wh-details__label">Warehouse Address:</h4>
+          <p className="wh-details__text wh-details__text--address">
+            33 Pearl Street SW, Washington, USA
+          </p>
         </div>
-        <div className="wh-etails__contact">
+        <div className="wh-details__contact">
           <div className="wh-details__contact-name">
-            <h4>Contact Name:</h4>
-            <p>Graeme Lyon</p>
-            <p>Warehouse Manager</p>
+            <h4 className="wh-details__label">Contact Name:</h4>
+            <p className="wh-details__text">Graeme Lyon</p>
+            <p className="wh-details__text">Warehouse Manager</p>
           </div>
           <div className="wh-details__contact-info">
-            <h4>Contact Information:</h4>
-            <p>453454353443</p>
-            <p>glyon@instock.com</p>
+            <h4 className="wh-details__label">Contact Information:</h4>
+            <p className="wh-details__text">+1 (647) 504-0911</p>
+            <p className="wh-details__text">glyon@instock.com</p>
           </div>
         </div>
       </div>
