cb8bef62183e879af0859544e8054f18a84eba26
Laura Nguyen <hello@lauranguyen.me>
2024-06-26 10:56:36 -0400

    added icons folder

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,36 +1,35 @@
 import "./WarehouseDetails.scss";
+import { Link } from "react-router-dom";
+import backArrow from "../../assets/icons/arrow_back-24px.svg";
 
 const WarehouseDetails = () => {
   return (
     <main className="wrapper">
-      <div className="edit-wh__header">
-        <Link className="edit-wh__back-link" to={"/warehouses"}>
+      <div className="wh-details__header">
+        <Link className="wh-details__header-btn--back" to={"/warehouses"}>
           <img
-            className="edit-wh__back-icon"
+            className="wh-details__header-icon--back"
             src={backArrow}
             alt="back arrow"
           />
         </Link>
-        <h1 className="edit-wh__title">Edit Warehouse</h1>
+        <h1 className="edit-wh__title">Washington</h1>
+        <button className="edit-wh__header-btn--edit"></button>
       </div>
       <hr className="edit-wh__divider" />
-      <div className="header">
-        <button className="header__button--back"></button>
-        <h1 className="header__name">Washington</h1>
-        <button className="header__button--edit"></button>
-      </div>
-      <div className="details">
-        <div className="details__address">
+
+      <div className="wh-details__wrapper">
+        <div className="wh-details__address">
           <h4>Warehouse Address:</h4>
           <p>Washington, USA</p>
         </div>
-        <div className="details__contact">
-          <div className="details__contact-name">
+        <div className="wh-etails__contact">
+          <div className="wh-details__contact-name">
             <h4>Contact Name:</h4>
             <p>Graeme Lyon</p>
             <p>Warehouse Manager</p>
           </div>
-          <div className="details__contact__info">
+          <div className="wh-details__contact-info">
             <h4>Contact Information:</h4>
             <p>453454353443</p>
             <p>glyon@instock.com</p>
