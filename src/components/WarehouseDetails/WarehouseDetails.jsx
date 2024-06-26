5afa3ce7d3ae01f7042f918b45eca5a894e1cdee
Laura Nguyen <hello@lauranguyen.me>
2024-06-26 10:24:01 -0400

    added html structure on react component

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,7 +1,33 @@
 import "./WarehouseDetails.scss";
 
 const WarehouseDetails = () => {
-  return <main></main>;
+  return (
+    <main className="wrapper">
+      <div className="header">
+        <button className="header__button--back"></button>
+        <h1 className="header__name">Washington</h1>
+        <button className="header__button--edit"></button>
+      </div>
+      <div className="details">
+        <div className="details__address">
+          <h4>Warehouse Address:</h4>
+          <p>Washington, USA</p>
+        </div>
+        <div className="details__contact">
+          <div className="details__contact-name">
+            <h4>Contact Name:</h4>
+            <p>Graeme Lyon</p>
+            <p>Warehouse Manager</p>
+          </div>
+          <div className="details__contact__info">
+            <h4>Contact Information:</h4>
+            <p>453454353443</p>
+            <p>glyon@instock.com</p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default WarehouseDetails;
