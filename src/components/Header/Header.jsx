d3e141b8dc331b1af3ea39536d40b324629d9077
isabella <isa99fv@gmail.com>
2024-06-30 11:02:22 -0400

    warehouses nav link now active at / route and /warehouses route

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,40 @@
-import "./Header.scss"
-import logo from "../../assets/logo/InStock-Logo.svg"
-import { Link, NavLink } from "react-router-dom";
+import "./Header.scss";
+import logo from "../../assets/logo/InStock-Logo.svg";
+import { Link, NavLink, useLocation } from "react-router-dom";
 
 const Header = () => {
+  const location = useLocation();
+
   return (
     <header className="header__background">
       <div className="header">
-      <Link to={"/warehouses"} className="header__logo">
-        <img src={logo} alt="instock logo" />
-      </Link>
-      <nav className="nav">
-        <NavLink className={({ isActive }) => "nav__item" + (isActive ? " nav__item--active" : "")} to={"/warehouses"}>Warehouses</NavLink>
-        <NavLink className={({ isActive }) => "nav__item" + (isActive ? " nav__item--active" : "")} to={"/inventories"}>Inventory</NavLink>
-      </nav>
+        <Link to={"/warehouses"} className="header__logo">
+          <img src={logo} alt="instock logo" />
+        </Link>
+        <nav className="nav">
+          <NavLink
+            className={({ isActive }) =>
+              "nav__item" +
+              (isActive || location.pathname === "/"
+                ? " nav__item--active"
+                : "")
+            }
+            to={"/warehouses"}
+          >
+            Warehouses
+          </NavLink>
+          <NavLink
+            className={({ isActive }) =>
+              "nav__item" + (isActive ? " nav__item--active" : "")
+            }
+            to={"/inventories"}
+          >
+            Inventory
+          </NavLink>
+        </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
